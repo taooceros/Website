91c052e7c3b97698cc2da2107c148208859bf520
Hongtao Zhang <hongtao_zhang@outlook.com>
2023-03-17 13:43:56 -0500

    fix and revise routing and update gitignore

--- Program.cs
+++ Program.cs
@@ -12,19 +12,17 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient
-{
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
-    DefaultRequestVersion = HttpVersion.Version20,
-    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower
-}).AddSingleton(_ => new MarkdownPipelineBuilder()
-    .UseTaskLists()
-    .UseEmphasisExtras()
-    .Build())
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+        DefaultRequestVersion = HttpVersion.Version20,
+        DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower
+    }).AddSingleton(_ => new MarkdownPipelineBuilder()
+        .UseTaskLists()
+        .UseEmphasisExtras()
+        .Build())
     .AddScoped<IDiaryService, DiaryService>()
     .AddScoped<IBlogService, BlogService>()
     .AddMudServices()
     .AddBlazoredLocalStorageAsSingleton();
-
-
 
 await builder.Build().RunAsync();
